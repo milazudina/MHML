c2c35a4183a1d44f1794bcafc5710aa62d949beb
Alex Gourlay <alexdgourlay@gmail.com>
2019-02-11 21:34:06 +0000

    Added bottom navigation
    Added a bottom navigation bar that is part of "activity_main" layout. Selecting each item in the bar loads the respective fragment onto the screen. Placeholder fragment layouts have been added. Icons from the Android vector library have been used to populate the bar. Menu items are stored in menu directory.

--- VitaRun/app/src/main/java/com/example/vitarun/MainActivity.java
+++ VitaRun/app/src/main/java/com/example/vitarun/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.vitarun;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.master_fragment_container,
+                new DashboardFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_dashboard:
+                            selectedFragment = new DashboardFragment();
+                            break;
+                        case R.id.nav_run:
+                            selectedFragment = new RunFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.master_fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
