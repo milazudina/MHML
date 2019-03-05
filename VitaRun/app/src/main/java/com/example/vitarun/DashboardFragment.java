ca613d50b48046375ab4d8ee3623ea51f1a1921e
bacon274 <jacob.fintan.mitchell.274@gmail.com>
2019-03-05 17:21:24 +0000

    AnyCharts

--- VitaRun/app/src/main/java/com/example/vitarun/DashboardFragment.java
+++ VitaRun/app/src/main/java/com/example/vitarun/DashboardFragment.java
@@ -7,12 +7,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.anychart.anychart.AnyChart;
+import com.anychart.anychart.AnyChartView;
+import com.anychart.anychart.DataEntry;
+import com.anychart.anychart.Pie;
+import com.anychart.anychart.ValueDataEntry;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardFragment extends Fragment {
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+
+        pie.setData(data);
+        AnyChartView anyChartView = (AnyChartView) view.findViewById(R.id.any_chart_view);
+        anyChartView.setChart(pie);
+
+//        return anyChartView;
+        return view;
     }
+
 }
