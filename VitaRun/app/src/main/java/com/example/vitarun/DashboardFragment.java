69ac0d65579e31c1f70fff83a10df11496077960
bacon274 <jacob.fintan.mitchell.274@gmail.com>
2019-03-07 19:23:56 +0000

    Graph and Calendar

--- VitaRun/app/src/main/java/com/example/vitarun/DashboardFragment.java
+++ VitaRun/app/src/main/java/com/example/vitarun/DashboardFragment.java
@@ -12,8 +12,15 @@ import com.anychart.anychart.AnyChartView;
 import com.anychart.anychart.DataEntry;
 import com.anychart.anychart.Pie;
 import com.anychart.anychart.ValueDataEntry;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 public class DashboardFragment extends Fragment {
 
@@ -23,19 +30,33 @@ public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        Pie pie = AnyChart.pie();
 
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("John", 10000));
-        data.add(new ValueDataEntry("Jake", 12000));
-        data.add(new ValueDataEntry("Peter", 18000));
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
 
-        pie.setData(data);
-        AnyChartView anyChartView = (AnyChartView) view.findViewById(R.id.any_chart_view);
-        anyChartView.setChart(pie);
 
-//        return anyChartView;
+
+        //        Pie pie = AnyChart.pie();
+//
+//        List<DataEntry> data = new ArrayList<>();
+//        data.add(new ValueDataEntry("John", 10000));
+//        data.add(new ValueDataEntry("Jake", 12000));
+//        data.add(new ValueDataEntry("Peter", 18000));
+//
+//        pie.setData(data);
+//        AnyChartView anyChartView = (AnyChartView) view.findViewById(R.id.any_chart_view);
+//        anyChartView.setChart(pie);
+
+
         return view;
     }
+
 
 }
