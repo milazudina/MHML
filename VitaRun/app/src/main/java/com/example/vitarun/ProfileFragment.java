fd33d87ec3c84c184b80bfb14f225971798e53b5
BenPheifer <benpheifer@hotmail.com>
2019-03-08 16:05:58 +0000

    Profile Switcher Implemented

--- VitaRun/app/src/main/java/com/example/vitarun/ProfileFragment.java
+++ VitaRun/app/src/main/java/com/example/vitarun/ProfileFragment.java
@@ -7,12 +7,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ViewSwitcher;
 
 public class ProfileFragment extends Fragment {
+    ViewSwitcher mViewSwitcher;
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        mViewSwitcher = view.findViewById(R.id.swProfileEdit);
+
+        Button editButton = (Button) view.findViewById(R.id.bEditProfile);
+        Button saveButton = (Button) view.findViewById(R.id.bSaveProfile);
+
+        editButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v ) {
+                System.out.println("change view"); // some function here
+                mViewSwitcher.showNext();
+                System.out.println("change view complete?");
+
+
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v ) {
+                System.out.println("change view"); // some function here
+                mViewSwitcher.showNext();
+                System.out.println("change view complete?");
+
+
+            }
+        });
+
+    return view;
+
     }
 }
