b4d623536ef8fbcb26b7e63ef2aa9843a98aa206
bacon274 <jacob.fintan.mitchell.274@gmail.com>
2019-03-09 14:59:48 +0000

    Graphing

--- VitaRun/app/src/main/java/com/example/vitarun/DashboardFragment.java
+++ VitaRun/app/src/main/java/com/example/vitarun/DashboardFragment.java
@@ -1,25 +1,32 @@
 package com.example.vitarun;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.anychart.anychart.AnyChart;
 import com.anychart.anychart.AnyChartView;
 import com.anychart.anychart.DataEntry;
 import com.anychart.anychart.Pie;
 import com.anychart.anychart.ValueDataEntry;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-
+import java.util.Locale;
 
 
 public class DashboardFragment extends Fragment {
@@ -30,29 +37,52 @@ public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-
-        GraphView graph = (GraphView) view.findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+        // Create Graph
+        final GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        // NEED TO GET REQUEST DATA FROM SERVER
+        // Set data input
+            // Need to be datetime
+        final BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
                 new DataPoint(0, 1),
+                new DataPoint(0, 0.5),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
                 new DataPoint(3, 2),
                 new DataPoint(4, 6)
         });
+
+        // set month view on graph
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(4);
         graph.addSeries(series);
 
+        // Create Calendar:
 
+        final CompactCalendarView compactCalendarView = (CompactCalendarView) view.findViewById(R.id.compactcalendar_view);
+        final SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
+        final TextView monthTextView = (TextView) view.findViewById(R.id.Month_title);
 
-        //        Pie pie = AnyChart.pie();
-//
-//        List<DataEntry> data = new ArrayList<>();
-//        data.add(new ValueDataEntry("John", 10000));
-//        data.add(new ValueDataEntry("Jake", 12000));
-//        data.add(new ValueDataEntry("Peter", 18000));
-//
-//        pie.setData(data);
-//        AnyChartView anyChartView = (AnyChartView) view.findViewById(R.id.any_chart_view);
-//        anyChartView.setChart(pie);
+        monthTextView.setText(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
+        // Calendar Listeners
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                System.out.println("date clicked");
+                graph.getViewport().setMinX(1);
+                graph.getViewport().setMaxX(3);
+                graph.addSeries(series);
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                System.out.println("month scrolled");
+                //  System.out.println(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
+                monthTextView.setText(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
+
+            }
+
+        });
 
 
         return view;
