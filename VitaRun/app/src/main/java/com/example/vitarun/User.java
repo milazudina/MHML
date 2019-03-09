73b83d3ef75647d0b71c099acd2cbb8211dddafc
BenPheifer <benpheifer@hotmail.com>
2019-03-09 15:37:18 +0000

    Create User.java

---
+++ VitaRun/app/src/main/java/com/example/vitarun/User.java
@@ -0,0 +1,16 @@
+package com.example.vitarun;
+
+public class User {
+
+    String username, password, nickname;
+    int weight, age;
+
+    public User(String username, String password, String nickname, int age, int weight) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.age = -1;
+        this.weight = -1;
+    }
+
+}
