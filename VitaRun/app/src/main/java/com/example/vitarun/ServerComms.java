800aa51347985285ce1afe86ca7a4650ed62aee7
Alex Gourlay <alexdgourlay@gmail.com>
2019-03-10 20:09:59 +0000

    Change pressureData data type

--- VitaRun/app/src/main/java/com/example/vitarun/ServerComms.java
+++ VitaRun/app/src/main/java/com/example/vitarun/ServerComms.java
@@ -228,7 +228,7 @@ public class ServerComms {
     }
 
 
-    public void PostPressureData(HashMap<String, String[]> data)
+    public void PostPressureData(HashMap<String, double[]> data)
     {
         OkHttpClient client = new OkHttpClient();
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
