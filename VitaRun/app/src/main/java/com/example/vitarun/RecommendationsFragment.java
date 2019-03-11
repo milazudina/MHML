751190cd23427da6567164cab6c5246fbe0d8cd8
Kenza <kz815@ic.ac.uk>
2019-03-11 16:48:44 +0000

    recomfrag update

--- VitaRun/app/src/main/java/com/example/vitarun/RecommendationsFragment.java
+++ VitaRun/app/src/main/java/com/example/vitarun/RecommendationsFragment.java
@@ -13,31 +13,63 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RecommendationsFragment extends Fragment {
 
+    Gson gson = new Gson();
 
 
-    CharSequence input = "Overpronation";
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        View RootView = inflater.inflate(R.layout.fragment_recommendations, container, false);
 
+        String request = "{'type': 1, 'freq': 180, 'totalNum': 73}";
+
 
         TextView tv = (TextView)RootView.findViewById(R.id.firstrecom_body);
-        tv.setText(input);
+
+
+        Summary_recom summary_recom = gson.fromJson(request , Summary_recom.class);
+        System.out.println(summary_recom.type);
+
+
+
+        switch (summary_recom.type) {
+            case "0":
+                tv.setText("Normal Pronation");
+                break;
+            case "1":
+                tv.setText("UnderPronation");
+                break;
+            case "2":
+                tv.setText("OverPronation");
+                break;
+        }
 
 //
         return RootView;
     }
 
+    private class Summary_recom {
+        public String type;
+        public String freq;
+        public String totalNum;
 
-
+        public Summary_recom(String type, String freq, String totalNum) {
+            this.type = type;
+            this.freq = freq;
+            this.totalNum = totalNum;
+        }
+    }
 
 
 
